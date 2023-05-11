03efd11c0fdafa275c82e602dc2fc55bc641931c
Никита Овсянников <124305580+mafaga00@users.noreply.github.com>
2023-05-11 23:11:40 +0300

    Практическая работа по 19 модулю.

--- API_test_19.7/settings.py
+++ API_test_19.7/settings.py
@@ -1,5 +1,5 @@
-valid_email = "mafaga00@yandex.ru"
-valid_password = "123654Fq"
+valid_email = "test00@yandex.ru"
+valid_password = "123654"
 
 invalid_email = "this_is_email"
 invalid_password = "this_is_password"
